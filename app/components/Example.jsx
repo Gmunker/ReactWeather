d62e05b65047094a70d994e0ef7993f3c9f7e8d9
Greg Munker <gmunker@munkerdesigns.com>
2016-09-20 13:36:54 -0400

    added copy

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Example = (props) => {
     return (
-        <h3>New Example Component!</h3>
+        <div>
+            <h3>New Example Component!</h3>
+            <p>Welcome to the Examples page</p>         
+        </div>
     )     
 }
 
