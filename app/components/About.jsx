c0a5cc27d34ec87aee488bd57e4c3c658efc989e
Greg Munker <gmunker@munkerdesigns.com>
2016-09-21 09:55:24 -0400

    About Page update

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,9 +2,24 @@ var React = require('react');
 
 var About = (props) => {
     return (
-        <div>
-            <h3>About</h3>
-            <p>Welcome to the about page!</p>
+        <div className="row">
+            <h1 className="text-center">About</h1>
+            <p>This is my first full ReactJS project, It also is the first first project to be commited to GitHub and Heroku. This is just another step in the right direction as a web developer!</p>
+            <p>Please visit my <a href="http://www.munkerdesigns.com" target="_blank" >personal page (munkerDesigns.com)</a> which will be getting updated after I learn more about ReactJS.</p>
+            <h5>Languages and Tools I used in this project include:</h5>
+            <ul>
+                <li>Javascript</li>
+                <li><a href="https://facebook.github.io/react" target="_blank" >ReactJS</a></li>
+                <li><a href="http://openweathermap.org" target="_blank" >Open Weather Map</a></li>
+                <li>Node.js</li>
+                <li>Babel</li>
+                <li>jQuery</li>
+                <li>Foundation</li>
+                <li>Git</li>
+                <li>GitHub</li>
+                <li>Command Line</li>
+                <li>Heroku</li>
+            </ul>
         </div>
     )
 };
